6de238e33cb68706ce58e7b1a66cd641de122e2c
Haikhal Fakhreez <ekaliacid@Ekals-MacBook-Pro.local>
2022-02-10 13:12:50 +0800

    update font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,6 @@
+import '@fontsource/dm-sans/400.css'
+import '@fontsource/dm-sans/500.css'
+import '@fontsource/dm-sans/700.css'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/layout'
