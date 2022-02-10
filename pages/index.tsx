8843f0d24c8fc7716e8b2793437c737b6332fe39
Haikhal Fakhreez <ekaliacid@Ekals-MacBook-Pro.local>
2022-02-10 12:19:36 +0800

    update README

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ const Home: NextPage = () => {
         <meta name="keywords" content="Portfolio, Haikhal Fakhreez, Web developer, Front-end" />
 
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://haikhalfakhreez.com" />
+        <meta property="og:url" content="https://www.haikhalfakhreez.com/" />
         <meta property="og:title" content="Portfolio | Haikhal Fakhreez" />
         <meta property="og:description" content="Haikhal Fakhreez's personal website" />
         <meta property="og:image" content="/haikhal-portfolio.png" />
@@ -30,7 +30,7 @@ const Home: NextPage = () => {
         <meta name="twitter:image" content="/haikhal-portfolio.png" />
         <meta name="twitter:alt" content="Portfolio | Haikhal Fakhreez" />
 
-        <link rel="canonical" href="https://haikhalfakhreez.com" />
+        <link rel="canonical" href="https://www.haikhalfakhreez.com/" />
         <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-16x16.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
