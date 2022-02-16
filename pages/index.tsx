db2ee0c158459160010b883f8c4a90a8badd98c7
Haikhal Fakhreez <ekaliacid@Ekals-MacBook-Pro.local>
2022-02-17 03:29:49 +0800

    fix favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,7 +32,7 @@ const Home: NextPage = () => {
 
         <link rel="canonical" href="https://www.haikhalfakhreez.com/" />
         <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
