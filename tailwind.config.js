8359671dbb8191065e5ba94c3666b309cafdae03
Haikhal Fakhreez <ekaliacid@Ekals-MacBook-Pro.local>
2022-02-18 02:58:24 +0800

    host grid image on server

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,7 @@ module.exports = {
         secondary: '#687076'
       },
       backgroundImage: {
-        'grid-image': `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32' fill='none' stroke='%23f1f5f9'%3e%3cpath d='M0 .5H31.5V32'/%3e%3c/svg%3e")`
+        'grid-image': `url("/grid.svg")`
       },
       backgroundPosition: {
         '10-10': '10px 10px'
